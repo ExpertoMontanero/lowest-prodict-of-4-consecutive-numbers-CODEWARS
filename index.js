536ed0631c84139740fe668d02c0e40fdbb88cc5
Tomasz Zawadzki <103272474+ExpertoMontanero@users.noreply.github.com>
2023-05-07 16:19:24 +0200

    Update index.js
    Code specification

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function lowestProduct(input) { 
+//making an input as an array of intigers
 input.split("").forEach(element=>parseInt(element));
+  //checking if array length is grater than 4 or returning proper info
   if(input.length<4){
     return "Number is too small";
   }
+  //looking for lowest product 
   var temp=0;
   var  min = input[0]*input[1]*input[2]*input[3];
   for(var index = 0; index<input.length-3; index++){
@@ -11,5 +14,6 @@ input.split("").forEach(element=>parseInt(element));
       min=temp;
     }
   }
+  //returning lowest product 
   return min;
 }
